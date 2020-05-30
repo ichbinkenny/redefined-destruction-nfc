76c695620049974cbfc53914ccfa63d0e0cf16f0
Kenneth Hunter <kennethdhunter@gmail.com>
2020-05-30 16:12:55 +0100

    NFC tags can now be scanned, and their UID displayed using make run.

---
+++ Makefile
@@ -0,0 +1,13 @@
+LIBS = -lnfc
+main:
+	gcc -o readNFCTag ./scanNFCTag.c $(LIBS)
+
+run:
+	gcc -o readNFCTag ./scanNFCTag.c $(LIBS)
+	./readNFCTag
+
+uidRun:
+	./readNFCTag
+
+devInfo:
+	./readNFCTag -i info
---
+++ scanNFCTag.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <nfc/nfc.h>
+
+void infoToHex(uint8_t* data, size_t size){
+    for(size_t pos = 0; pos < size; pos++){
+        printf("%02x", data[pos]);
+    }
+}
+
+int main(int argc, char* argv[]){
+    nfc_device* dev;
+    nfc_context* context;
+    nfc_target nfcTarget;
+    nfc_init(&context);
+    if(context == NULL){
+        perror("Failed to open nfc reader!\r\n");
+        exit(EXIT_FAILURE);
+    }
+    dev = nfc_open(context, NULL);
+    if(dev == NULL){
+        perror("Failed to connect to nfc reader!\r\n");
+        exit(EXIT_FAILURE);
+    }
+    if(nfc_initiator_init(dev) < 0){
+        perror("Failed to setup nfc reader!\r\n");
+        exit(EXIT_FAILURE);
+    }
+    const nfc_modulation nmMifare = {
+    .nmt = NMT_ISO14443A,
+    .nbr = NBR_106,
+    };
+    printf("Please scan your NFC tag...\r\n");
+    if(nfc_initiator_select_passive_target(dev, nmMifare, NULL, 0, &nfcTarget) > 0){
+        if(argc <= 2){
+            printf("UID: ");
+            infoToHex(nfcTarget.nti.nai.abtUid, nfcTarget.nti.nai.szUidLen);
+            printf("\r\n");
+        }
+        else{
+            printf("Dev info: ");
+        }
+    }
+    nfc_close(dev);
+    nfc_exit(context);
+    exit(EXIT_SUCCESS);
+}
